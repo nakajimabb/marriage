d354ac1e2965b80cafd600b2d7581ceea06bb242
中島真一 <nakajimabb@gmail.com>
2019-10-31 17:00:48 +0900

    set locale before action

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,4 +1,8 @@
 class ApplicationController < ActionController::API
   include DeviseTokenAuth::Concerns::SetUserByToken
-  before_action :authenticate_user!
+  before_action :authenticate_user!, :set_locale
+
+  def set_locale
+    I18n.locale = current_user&.lang || I18n.default_locale
+  end
 end
